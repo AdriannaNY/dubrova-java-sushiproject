495d95a0b79b0deda4c7186090e0b8904740c490
Ekaterina Dubrova Mac <dubrova.ekaterina060516@yandex.ru>
2023-11-01 18:32:48 +0100

    created new project DUBROVA-JAVA-SUSHIPROJECT

---
+++ src/main/java/com/starta/DUBROVAJAVASUSHIPROJECT/DubrovaJavaSushiprojectApplication.java
@@ -0,0 +1,13 @@
+package com.starta.DUBROVAJAVASUSHIPROJECT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubrovaJavaSushiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubrovaJavaSushiprojectApplication.class, args);
+	}
+
+}
